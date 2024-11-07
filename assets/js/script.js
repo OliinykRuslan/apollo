a8a48f50fd5b6d519d67d1e3aea4f06fe782abe3
DESKTOP-RLGH462\Work Space <oliinykruslan@gmail.com>
2024-11-07 16:22:03 +0200

    langing page with Apollo Chat Interface

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+(function(d, t) {
+    var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+    v.onload = function() {
+        window.voiceflow.chat.load({
+            verify: {
+            projectID: '66830678ac70dd42bf0d143d'
+        },
+        url: 'https://general-runtime.voiceflow.com',
+        versionID: 'production',
+        render: {
+            mode: 'embedded',
+            target: document.getElementById('flat-chat')
+        },
+        autostart: true
+        });
+    }
+    v.src = "https://cdn.voiceflow.com/widget/bundle.mjs";
+    v.type = "text/javascript";
+    s.parentNode.insertBefore(v, s);
+})(document, 'script');
+
+//start-btn
+document.querySelector('.start-btn').addEventListener('click', function() {
+    const mainBlock = document.querySelector('.main');
+    const mainChatBlock = document.querySelector('.main-chat');
+
+    if (mainBlock && mainChatBlock) {
+        mainBlock.classList.add('hide');
+        mainChatBlock.classList.add('active');
+    }
+});
+
+//cart
+document.querySelector('.chat-cart-icon').addEventListener('click', function() {
+    document.querySelector('.cart-wrap').classList.add('show');
+});
+document.querySelector('.cart-wrap .close-btn').addEventListener('click', function() {
+    document.querySelector('.cart-wrap').classList.remove('show');
+});
+document.querySelector('.cart-overlay').addEventListener('click', function() {
+    document.querySelector('.cart-wrap').classList.remove('show');
+});
