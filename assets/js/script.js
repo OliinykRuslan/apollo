b127ca3ee56f41aaeec52b4198cfc8f37937593b
DESKTOP-RLGH462\Work Space <oliinykruslan@gmail.com>
2024-11-08 23:35:02 +0200

    working with cart

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,6 @@
 })(document, 'script');
 
 //start-btn
-
 let mainBlock = document.querySelector('.main');
 let mainChatBlock = document.querySelector('.main-chat');
 document.querySelector('.start-btn').addEventListener('click', function() {
@@ -53,3 +52,63 @@ document.querySelector('.cart-wrap .close-btn').addEventListener('click', functi
 document.querySelector('.cart-overlay').addEventListener('click', function() {
     document.querySelector('.cart-wrap').classList.remove('show');
 });
+
+//
+// fetch('https://clinicalroots.azurewebsites.net/wp-json/custom/v1/cart', {
+//     headers: {
+//         'Authorization': 'Basic ' + btoa('ck_33cb63a110351082ba0ee871261b056685357d78:cs_8bc9263b74f0875c3ca631b9fd0371c7b80f92f4')
+//     }
+// })
+// .then(response => response.json())
+// .then(data => {
+//     console.log(data);
+//     // Відображення товарів у кошику на вашому лендінгу
+//     displayCartItems(data);
+// })
+// .catch(error => console.error('Error:', error));
+
+// function displayCartItems(items) {
+//     const cartContainer = document.getElementById("cart");
+//     items.forEach(item => {
+//         cartContainer.innerHTML += `
+//             <div class="product">
+//                 <img src="${item.image}" alt="${item.name}">
+//                 <h3>${item.name}</h3>
+//                 <p>Ціна: ${item.price}</p>
+//                 <p>Кількість: ${item.quantity}</p>
+//             </div>
+//         `;
+//     });
+// }
+
+
+// 2
+// async function fetchCartData() {
+//     try {
+//         const response = await fetch('https://clinicalroots.azurewebsites.net/wp-json/custom/v1/cart');
+//         const cartData = await response.json();
+//         console.log(cartData); // Вивести дані кошика в консоль
+//         // Додайте логіку для відображення даних кошика на сторінці
+//     } catch (error) {
+//         console.error('Error fetching cart data:', error);
+//     }
+// }
+
+// fetchCartData();
+
+// 3
+async function fetchCartData() {
+    try {
+        const response = await fetch('https://clinicalroots.azurewebsites.net/wp-json/custom/v1/cart', {
+            headers: {
+                'Authorization': 'Basic ' + btoa('ck_33cb63a110351082ba0ee871261b056685357d78:cs_8bc9263b74f0875c3ca631b9fd0371c7b80f92f4')
+            }
+        });
+        const cartData = await response.json();
+        console.log(cartData);
+    } catch (error) {
+        console.error('Error fetching cart data:', error);
+    }
+}
+
+fetchCartData();
